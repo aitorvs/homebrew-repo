db70277e5912730295a35336a20ee5950332bffd
Aitor Viana <aitorvs@gmail.com>
2021-03-07 11:18:57 +0100

    beeline 0.3.0 (#2)
    Co-authored-by: github-actions <41898282+github-actions@users.noreply.github.com>

--- Formula/beeline.rb
+++ Formula/beeline.rb
@@ -1,9 +1,9 @@
 class Beeline < Formula
   desc "A tool for creating network routes that exclude given subnets"
   homepage "https://github.com/aitorvs/beeline"
-  url "https://github.com/aitorvs/beeline/releases/download/0.2.0/beeline-0.2.0-binary.jar"
-  version "0.2.0"
-  sha256 "c9a1cfeb0637160240c2e223937a59c78b2ac0d731f0d050491fd5080ebe558e"
+  url "https://github.com/aitorvs/beeline/releases/download/0.3.0/beeline-0.3.0-binary.jar"
+  version "0.3.0"
+  sha256 "19523ba0d8cbd32b069b71e8fef4187cda61e379054faac72f31abd430ea46a0"
 
   bottle :unneeded
 
