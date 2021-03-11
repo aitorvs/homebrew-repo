0310f37535788ca98e3174b29319cc53c358bb0d
Aitor Viana <aitorvs@gmail.com>
2021-03-11 01:50:30 +0100

    elgato 0.2.3 (#8)
    Co-authored-by: github-actions <41898282+github-actions@users.noreply.github.com>

--- Formula/elgato.rb
+++ Formula/elgato.rb
@@ -1,9 +1,9 @@
 class Elgato < Formula
   desc "A command line too to control the elgato key lights"
   homepage "https://github.com/aitorvs/elgato"
-  url "https://github.com/aitorvs/elgato/releases/download/0.2.1/elgato-0.2.1-binary.jar"
-  version "0.2.1"
-  sha256 "0e5de9d4de639575929e3905afe663ebcebf9cd55a66d2ef7912cf9a77a51163"
+  url "https://github.com/aitorvs/elgato/releases/download/0.2.3/elgato-0.2.3-binary.jar"
+  version "0.2.3"
+  sha256 "e60a3b169b69d5fd574677992db4f9832ad18566158e191cb736349937d71aa5"
 
   bottle :unneeded
 
